571b68ec52fd1791b6a8ea8ba7b2aa510c4ce777
Kunalmighty <kmaiti1@jhu.edu>
2020-10-17 18:29:34 -0400

    Database updates successful!

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 mongoose
   .connect(
     "mongodb+srv://kunal:123@cluster0.bmecr.mongodb.net/Cluster0?retryWrites=true&w=majority",
-    { useMongoClient: true, useNewUrlParser: true, useUnifiedTopology: true }
+    { useNewUrlParser: true, useUnifiedTopology: true }
   )
   .then(() => console.log("db connected"));
 
@@ -59,7 +59,7 @@ app.post("/inbound", (req, res) => {
       newCustomer.phoneNumber = from;
       newCustomer.nextAppointment = null;
       newCustomer.lastVaccination = null;
-      newCustomer.inConversation1 = false;
+      newCustomer.inConversation1 = true;
       newCustomer.inConversation2 = false;
       newCustomer.save(() => {
         client.messages
@@ -154,10 +154,20 @@ app.post("/inbound", (req, res) => {
               "Here are your available time slots:\n1. Monday October 26th, 6 PM\n2. Tuesday October 27th, 5 PM\n3. Wednesday October 28th, 2:30 PM\nPlease text 1, 2 or 3 for which time slot you would like to reserve.",
           })
           .then((message) => console.log(message.sid));
+          console.log(customers[0]._id);
         Customer.findByIdAndUpdate(
           customers[0]._id,
           { "$set": { "inConversation2": true } },
-          { "new": true, "upsert": true }
+          { "new": true, "upsert": true },
+          function(err, result) {
+            if (err) {
+              console.log("Database update NOT successful!\n")
+              console.log(err);
+            } else {
+              console.log("Database update successful!\n")
+              console.log(result);
+            }
+          }
         );
         res.end();
       } else if (body === "2") {
@@ -169,9 +179,20 @@ app.post("/inbound", (req, res) => {
               "Your appointment has been deleted. Thank you for reaching out!",
           })
           .then((message) => console.log(message.sid));
-        Customer.findByIdAndUpdate(customers[0]._id, {
-          inConversation1: false,
-        });
+          Customer.findByIdAndUpdate(
+            customers[0]._id,
+            { "$set": { "inConversation1": false } },
+            { "new": true, "upsert": true },
+            function(err, result) {
+              if (err) {
+                console.log("Database update NOT successful!\n")
+                console.log(err);
+              } else {
+                console.log("Database update successful!\n")
+                console.log(result);
+              }
+            }
+          );
         res.end();
       }
     }

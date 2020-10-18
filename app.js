78ccc56a1c02b51e4cfe890bac62455fb418601e
Kunalmighty <kmaiti1@jhu.edu>
2020-10-17 20:00:42 -0400

    Successfully updating next appointment.

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.post("/inbound", (req, res) => {
 
   //Check if customer with this number exists
   Customer.find({ phoneNumber: req.body.From }, (err, customers) => {
-    //console.log(customers[0].inConversation1);
+
     //If the number does not exist and the body is Hello, RSVP, Woof or Meow
     if (
       customers.length === 0 &&
@@ -88,11 +88,12 @@ app.post("/inbound", (req, res) => {
           to: `${from}`,
           from: `${to}`,
           body:
-            "Invalid text. If you are a customer of VCA Animal Hospitals, please text Woof or Meow",
+            "Invalid text. If you are a customer of VCA Animal Hospitals, please text the word Hello, RSVP, Woof or Meow",
         })
         .then((message) => console.log(message.sid));
       res.end();
     }
+
     //If the number exists and the body is Hello, RSVP, Woof or Meow (set inConversation1 to true) -> false on success
     else if (
       customers.length !== 0 &&
@@ -116,6 +117,7 @@ app.post("/inbound", (req, res) => {
         .then((message) => console.log(message.sid));
       res.end();
     }
+
     //If the number exists and the body is not Hello, RSVP, Woof or Meow
     else if (
       customers.length !== 0 &&
@@ -151,13 +153,13 @@ app.post("/inbound", (req, res) => {
             to: `${from}`,
             from: `${to}`,
             body:
-              "Here are your available time slots:\n1. Monday October 26th, 6 PM\n2. Tuesday October 27th, 5 PM\n3. Wednesday October 28th, 2:30 PM\nPlease text 1, 2 or 3 for which time slot you would like to reserve.",
+              "Here are your available time slots:\n1. Monday October 26th, 6 PM\n2. Tuesday October 27th, 5 PM\n3. Wednesday October 28th, 2:30 PM\nPlease text 1, 2 or 3 for which time slot you would like to reserve, or text 4 to go back to the previous menu.",
           })
           .then((message) => console.log(message.sid));
           console.log(customers[0]._id);
         Customer.findByIdAndUpdate(
           customers[0]._id,
-          { "$set": { "inConversation2": true } },
+          { "$set": { "inConversation1": false, "inConversation2": true } },
           { "new": true, "upsert": true },
           function(err, result) {
             if (err) {
@@ -181,7 +183,7 @@ app.post("/inbound", (req, res) => {
           .then((message) => console.log(message.sid));
           Customer.findByIdAndUpdate(
             customers[0]._id,
-            { "$set": { "inConversation1": false } },
+            { "$set": { "nextAppointment": null, "inConversation1": false, "inConversation2": false } },
             { "new": true, "upsert": true },
             function(err, result) {
               if (err) {
@@ -197,28 +199,143 @@ app.post("/inbound", (req, res) => {
       }
     }
 
-    //If the number exists and we are inConversation1 and the body is not 1, 2 or 3
+    //If the number exists and we are inConversation1 and the body is not 1 or 2
+    else if (
+      customers.length !== 0 &&
+      customers[0].inConversation1 === true &&
+      !(
+        body === "1" ||
+        body === "2"
+      )
+    ) {
+      client.messages
+        .create({
+          to: `${from}`,
+          from: `${to}`,
+          body:
+            "Invalid message. Please text the number 1 or 2 to select your option!",
+        })
+        .then((message) => console.log(message.sid));
+      res.end();
+    }
+
     //If the number exists and we are inConversation2 and the body is 1, 2 or 3 (set inConversation2 to false)
-    //If the number exists and we are inConversation2 and the body is not 1, 2 or 3
-    else {
-      if (body === "RSVP") {
-        let newCustomer = new Customer();
-        newCustomer.phoneNumber = from;
-        newCustomer.nextAppointment = null;
-        newCustomer.lastVaccination = null;
-        newCustomer.inConversation1 = false;
-        newCustomer.inConversation2 = false;
-        newCustomer.save(() => {
-          client.messages
-            .create({
-              to: `${from}`,
-              from: `${to}`,
-              body: "What is your group name?",
-            })
-            .then((message) => console.log(message.sid));
-          res.end();
-        });
+    else if (
+      customers.length !== 0 &&
+      customers[0].inConversation2 === true &&
+      (body === "1" || body === "2" || body === "3")
+    ) {
+      if (body === "1") {
+        client.messages
+          .create({
+            to: `${from}`,
+            from: `${to}`,
+            body:
+              "Your appointment is scheduled for Monday October 26th at 6 PM. We'll see you then!",
+          })
+          .then((message) => console.log(message.sid));
+        Customer.findByIdAndUpdate(
+          customers[0]._id,
+          { "$set": { "nextAppointment": new Date('2020-10-26T18:00:00.001Z'), "inConversation1": false, "inConversation2": false } },
+          { "new": true, "upsert": true },
+          function(err, result) {
+            if (err) {
+              console.log("Database update NOT successful!\n")
+              console.log(err);
+            } else {
+              console.log("Database update successful!\n")
+              console.log(result);
+            }
+          }
+        );
+        res.end();
+      } else if (body === "2") {
+        client.messages
+          .create({
+            to: `${from}`,
+            from: `${to}`,
+            body:
+              "Your appointment is scheduled for Tuesday October 27th at 5 PM. We'll see you then!",
+          })
+          .then((message) => console.log(message.sid));
+          Customer.findByIdAndUpdate(
+            customers[0]._id,
+            { "$set": { "nextAppointment": new Date('2020-10-27T17:00:00.001Z'), "inConversation1": false, "inConversation2": false } },
+            { "new": true, "upsert": true },
+            function(err, result) {
+              if (err) {
+                console.log("Database update NOT successful!\n")
+                console.log(err);
+              } else {
+                console.log("Database update successful!\n")
+                console.log(result);
+              }
+            }
+          );
+        res.end();
+      } else if (body === "3") {
+        client.messages
+          .create({
+            to: `${from}`,
+            from: `${to}`,
+            body:
+              "Your appointment is scheduled for Wednesday October 28th at 2:30 PM. We'll see you then!",
+          })
+          .then((message) => console.log(message.sid));
+          Customer.findByIdAndUpdate(
+            customers[0]._id,
+            { "$set": { "nextAppointment": new Date('2020-10-28T14:30:00.001Z'), "inConversation1": false, "inConversation2": false } },
+            { "new": true, "upsert": true },
+            function(err, result) {
+              if (err) {
+                console.log("Database update NOT successful!\n")
+                console.log(err);
+              } else {
+                console.log("Database update successful!\n")
+                console.log(result);
+              }
+            }
+          );
+        res.end();
       }
+      else if (body === "4") {
+        client.messages
+          .create({
+            to: `${from}`,
+            from: `${to}`,
+            body:
+              "Welcome back to VCA Animal Hospitals. Please select one of these options:\nText 1 to create a new appointment or update existing appointment.\nText 2 to delete appointment.",
+          })
+          .then((message) => console.log(message.sid));
+          Customer.findByIdAndUpdate(
+            customers[0]._id,
+            { "$set": { "inConversation1": true, "inConversation2": false } },
+            { "new": true, "upsert": true },
+            function(err, result) {
+              if (err) {
+                console.log("Database update NOT successful!\n")
+                console.log(err);
+              } else {
+                console.log("Database update successful!\n")
+                console.log(result);
+              }
+            }
+          );
+        res.end();
+      }
+    }
+
+    //If the number exists and we are inConversation2 and the body is not 1, 2 or 3
+    else if (  customers.length !== 0 && customers[0].inConversation2 === true && !(body === "1" || body === "2" || body === "3" || body === "4")) {
+      client.messages
+        .create({
+          to: `${from}`,
+          from: `${to}`,
+          body:
+            "Invalid message. Please text the number 1, 2, 3 or 4 to select your option!",
+        })
+        .then((message) => console.log(message.sid));
+      res.end();
     }
 
     res.end();
